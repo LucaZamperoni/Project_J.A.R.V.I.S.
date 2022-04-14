0b0288beb38d830429b7ada7aa62774bf7525442
Luca Zamperoni <luca.zamperoniz@gmail.com>
2022-04-13 23:06:33 -0300

    Trying to use branch 2 ...

--- src/implement/Jarvis.java
+++ src/implement/Jarvis.java
@@ -30,6 +30,7 @@ De esa manera podria hacer que el metodo retorne algun valor y evitar el uso de 
 
 8. Agregar probabilidad de fallo al metodo para disparar al enemigo.
 9. PRUEBA GIT.
+10. Prueba branch 2.
  */
 public class Jarvis {
 
